f5e7d1299aa2e4d3b261f7659243ef6e65ed76d6
Troy McCall <troyxmccall@gmail.com>
2019-08-10 16:45:14 -0400

    update refs

--- goreleaser.sh
+++ goreleaser.sh
@@ -4,7 +4,7 @@ cd "${0%/*}"
 
 docker run \
 	--rm -ti --privileged \
-	-v $(pwd):/go/src/github.com/kamaln7/karmabot \
+	-v $(pwd):/go/src/github.com/troyxmccall/janet \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v $HOME/.config/goreleaser/github_token:/root/.config/goreleaser/github_token \
     -v $HOME/.docker/config.json:/root/.docker/config.json \
